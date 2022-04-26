f30f2ddd65bbc127a1a73b9ab21c0f5dc70bb5cd
Shawn Kebel <fxrender@outlook.com>
2022-04-26 14:14:47 -0500

    :technologist: Add `tempData` for sample data

---
+++ tempData.js
@@ -0,0 +1,70 @@
+// Temporary Data until connected with Firebase
+
+export default tempData = [
+  {
+    name: "Plan a Trip",
+    color: "#24a6d9",
+    todos: [
+      {
+        title: "Book Flights",
+        completed: false
+      },
+      {
+        title: "Passport Check",
+        completed: true
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true
+      },
+      {
+        title: "Pack Luggage",
+        completed: false
+      }
+    ]
+  },
+  {
+    name: "Errands",
+    color: "#8022d9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false
+      },
+      {
+        title: "Plan Weekend camping trip",
+        completed: true
+      },
+      {
+        title: "Run 2 miles",
+        completed: true
+      },
+      {
+        title: "Make a video",
+        completed: true
+      },
+      {
+        title: "Take dog for a walk",
+        completed: true
+      }
+    ]
+  },
+  {
+    name: "Birthday Party",
+    color:"#595b99",
+    todos: [
+      {
+        title: "Get Balloons",
+        completed: false
+      },
+      {
+        title: "Send Invitations",
+        completed: true
+      },
+      {
+        title: "Make Dinner Reservations",
+        completed: true
+      }
+    ]
+  }
+];
