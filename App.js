ccf088b1fa04475d7803dd8316353fe387417b51
Shawn Kebel <fxrender@outlook.com>
2022-04-26 14:13:03 -0500

    :tada: Start `TodoApp2.0` home screen

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from './Colors';
+import tempData from './tempData';
+import TodoList from './components/TodoList';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View style={{flexDirection: "row"}}>
+          <View style={styles.divider} />
+            <Text style={styles.title}>
+              Todo <Text style={{fontWeight: "300", color: colors.blue}}>Lists</Text>
+            </Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={{marginVertical: 48}}>
+
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add a list</Text>
+        </View>
+
+        <View style={{height: 275, paddingLeft: 32}}>
+          <FlatList 
+            data={tempData} 
+            keyExtractor={item => item.name} 
+            horizontal={true} 
+            showsHorizontalScrollIndicator={false} 
+            renderItem={({item}) => 
+              <TodoList list={item} />}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +49,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 64
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8
+  }
 });
+
