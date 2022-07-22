53a68992485fd96114a33f82d53fd6fe69821095
Shawn Kebel <fxrender@outlook.com>
2022-07-21 22:05:59 -0500

    :lipstick: Add `New Styles`

---
+++ newStyles/NewStyles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+// import { backgroundColor } from "react-native/Libraries/";
+
+export default StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  backgroundCover: {
+    alignSelf: "stretch",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000000",
+  },
+});
